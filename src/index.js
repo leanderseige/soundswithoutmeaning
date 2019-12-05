f52c1e904aa9c748593ad2427256caab15162427
Leander Seige <leander@seige.name>
2019-12-05 22:46:21 +0100

    polishing

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import store from './store'
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
 
+// https://gist.github.com/endel/321925f6cafa25bbfbde 
 Number.prototype.pad = function(size) {
   var s = String(this);
   while (s.length < (size || 2)) {s = "0" + s;}
